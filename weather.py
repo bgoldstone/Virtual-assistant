31a01f8fb488de3c160d3fda57e2920300980ffc
AnonymeKnight <tnizar019@gmail.com>
2020-09-12 16:32:36 +0100

    slight changes

--- weather.py
+++ weather.py
@@ -11,5 +11,5 @@ api_url = f"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={co
 #sending the request to the api and collecting informations
 weather_request = requests.get(api_url)
 data = weather_request.json()
-pprint(data["weather"]["main"])
+pprint(data)
 
