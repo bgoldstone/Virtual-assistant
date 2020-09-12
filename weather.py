42f80d2252bad6c6fbb3df49263a37cb0353e671
AnonymeKnight <tnizar019@gmail.com>
2020-09-12 17:56:19 +0100

    Accessed the weather data and fixed some problems

--- weather.py
+++ weather.py
@@ -4,12 +4,21 @@
 #import modules
 import config
 from pprint import pprint
+import json
 import requests
 
 api_url = f"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={config.api_key}"
 
 #sending the request to the api and collecting informations
-weather_request = requests.get(api_url)
-data = weather_request.json()
-pprint(data)
+response = requests.get(api_url)
+data = response.json()
+
+to_find = "weather"
+
+if type(data[to_find]) == list:
+    weather_data = data["weather"]
+    for i in range(len(weather_data)):
+        print(weather_data[i]["main"])
+        break
+
 
