4cc47f7faef3c877249a39f61eeaa84c6e07311b
AnonymeKnight <tnizar019@gmail.com>
2020-09-12 18:49:01 +0100

    Setting the base of the UI

--- rootui.py
+++ rootui.py
@@ -4,3 +4,17 @@
 import tkinter as tk
 
 #setting variables
+size = "480x360"
+
+
+
+
+
+#setting up the window
+root = tk.Tk()
+root.title("Virtual Assistant")
+root.geometry(size)
+
+
+#displaying the window
+root.mainloop()
